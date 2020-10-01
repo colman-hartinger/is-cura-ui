1812f0b10d7cc893899a1dc56a81f281f5f7326b
jake-kirkland-teton <62028020+jake-kirkland-teton@users.noreply.github.com>
2020-10-01 13:41:49 -0600

    Got tests running up to submitting/cancelling jobs

--- SmartSlicePlugin/tests/test_API.py
+++ SmartSlicePlugin/tests/test_API.py
@@ -10,6 +10,7 @@ class MockThor():
     def init(self):
         self._token = None
         self._active_connection = True
+        self._subscription = None
 
     def info(self):
         if self._active_connection:
@@ -34,8 +35,24 @@ class MockThor():
             self._token = "bad"
             return 404, None
 
+    def smartslice_subscription(self):
+        if self._subscription:
+            return 200, "active"
+        elif not self._subscription:
+            return 200, "inactive"
+        else:
+            return 429, None
+
+    def new_smartslice_job(self, threemf):
+        job = object
+        job.status = 102
+        return 200, job
+
     def get_token(self):
         return self._token
+
+    def set_token(self, token):
+        self._token = token
 
 class test_API(_SmartSliceTestCase):
     @classmethod
@@ -43,6 +60,7 @@ class test_API(_SmartSliceTestCase):
         from SmartSlicePlugin.SmartSliceCloudConnector import SmartSliceAPIClient
 
         mockConnector = MagicMock()
+        mockConnector.status = MagicMock()
         mockConnector.extension = MagicMock(MagicMock())
         mockConnector.extension.metadata = MagicMock()
         cls._api = SmartSliceAPIClient(mockConnector)
@@ -58,6 +76,7 @@ class test_API(_SmartSliceTestCase):
         self._api._login_username = None
         self._api._login_password = None
         self._api._client._active_connection = True
+        self._api._client._subscription = None
 
         self._api._app_preferences.removePreference(self._api._username_preference)
         self._api._app_preferences.addPreference(self._api._username_preference, "old@email.com")
@@ -65,9 +84,32 @@ class test_API(_SmartSliceTestCase):
     def tearDown(self):
         pass
 
-    def test_0_login_success(self):
+    def test_0_check_token_create(self):
+        self._api._token = "good"
+        self._api._createTokenFile()
+
+        self._api._token = "cleared"
+        self._api._getToken()
+
+        self.assertIsNotNone(self._api._token)
+        self.assertEqual(self._api._token, "good")
+
+    def test_1_check_token_good(self):
+        self._api._checkToken()
+
+        self.assertEqual(self._api._client._token, "good")
+
+    def test_2_check_token_bad(self):
+        self._api._token = None
+        self._api._checkToken()
+
+        self.assertNotEqual(self._api._client._token, "good")
+
+    def test_3_login_success(self):
         self._api._login_username = "good@email.com"
         self._api._login_password = "goodpass"
+
+        self.assertFalse(self._api.logged_in)
 
         self._api._login()
 
@@ -75,8 +117,9 @@ class test_API(_SmartSliceTestCase):
         self.assertEqual(self._api._login_password, "")
         self.assertEqual(self._api._app_preferences.getValue(self._api._username_preference), self._api._login_username)
         self.assertEqual(self._api._token, "good")
+        self.assertTrue(self._api.logged_in)
 
-    def test_1_login_credentials_failure(self):
+    def test_4_login_credentials_failure(self):
         self._api._login_username = "bad"
         self._api._login_password = "nopass"
 
@@ -86,8 +129,9 @@ class test_API(_SmartSliceTestCase):
         self.assertEqual(self._api._login_password, "")
         self.assertNotEqual(self._api._app_preferences.getValue(self._api._username_preference), self._api._login_username)
         self.assertIsNone(self._api._token)
+        self.assertFalse(self._api.logged_in)
 
-    def test_2_login_connection_failure(self):
+    def test_5_login_connection_failure(self):
         self._api._login_username = "good@email.com"
         self._api._login_password = "goodpass"
         self._api._client._active_connection = False
@@ -97,8 +141,9 @@ class test_API(_SmartSliceTestCase):
         self.assertIsNotNone(self._api._error_message)
         self.assertEqual(self._api._error_message.getText(), "Internet connection issue:\nPlease check your connection and try again.")
         self.assertTrue(self._api._error_message.visible)
+        self.assertFalse(self._api.logged_in)
 
-    def test_3_logout(self):
+    def test_6_logout(self):
         self._api._token = "good"
         self._api._loginPassword = "goodpass"
 
@@ -108,3 +153,33 @@ class test_API(_SmartSliceTestCase):
         self.assertIsNone(self._api._getToken())
         self.assertEqual(self._api._login_password, "")
         self.assertEqual(self._api._app_preferences.getValue(self._api._username_preference), "")
+
+    def test_7_subscription_active(self):
+        self._api._client._subscription = True
+
+        subscription = self._api.getSubscription()
+
+        self.assertEqual(subscription, "active")
+
+    def test_8_subscription_inactive(self):
+        self._api._client._subscription = False
+
+        subscription = self._api.getSubscription()
+
+        self.assertEqual(subscription, "inactive")
+
+    def test_9_submit_job_success(self):
+        #JobStatusTracker = MagicMock(MagicMock())
+        pass
+
+    def test_10_submit_job_fail(self):
+        pass
+
+    def test_11_submit_job_queued(self):
+        pass
+
+    def test_12_cancel_job_success(self):
+        pass
+
+    def test_13_cancel_job_fail(self):
+        pass
