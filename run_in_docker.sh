e9d997bfb8c49ac1dcf2ea3be72010bc178344ad
colman <colman.hartinger@tetonsim.com>
2020-10-05 17:05:43 -0600

    github action integration

---
+++ run_in_docker.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+git clone https://github.com/tetonsim/pywim.git
+git clone https://github.com/tetonsim/py3mf.git
+mv pywim/pywim SmartSlicePlugin/3rd-party/cpython-common
+mv py3mf/threemf SmartSlicePlugin/3rd-party/cpython-common
+
+git clone -b 4.6 --single-branch  https://github.com/Ultimaker/Cura.git
+mv SmartSlicePlugin Cura/plugins
+
+python3 -m pip install PyQt5==5.10
+
+# shellcheck disable=SC2164
+cd Cura
+PYTHONPATH=${PYTHONPATH}:$(pwd)
+
+Xvfb :1.0 -screen 0 1280x800x16 &
+export DISPLAY=:1.0
+export QT_QPA_PLATFORM=offscreen
+python3 plugins/SmartSlicePlugin/tests/run.py
